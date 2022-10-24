80c27cc2ff23205e82dd17789961a386c1eb0815
Juan del Moral <jdelm86@gmail.com>
2022-10-24 17:40:22 -0300

    trabajando en tercera entrega

---
+++ Desafio-3/products.js
@@ -0,0 +1,26 @@
+const productos = [
+    {
+        id: 1,
+        nombre: 'Torrontes',
+        precio: 3500,
+        img: "./img/torrontes-1.webp",
+    },
+    {
+        id: 2,
+        nombre: "Chardonnay",
+        precio: 3500,
+        img: "./img/chardonnay.webp",
+    },
+    {
+        id: 3,
+        nombre: "Malbec",
+        precio: 4000,
+        img: "./img/malbec.webp",
+    },
+    {
+        id: 3,
+        nombre: "Cabernet Sauvignon",
+        precio: 4000,
+        img: "./img/cabernet.webp",
+    },
+];
